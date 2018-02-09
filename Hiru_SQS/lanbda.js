c2143f0e9beb18eb019cfc5a3d984d83d400140c
hirudinee <35219641+hirudinee@users.noreply.github.com>
2018-02-09 13:09:35 +0530

    tet

--- Hiru_SQS/lanbda.js
+++ Hiru_SQS/lanbda.js
@@ -5,13 +5,13 @@ exports.handler = function (event, context, callback) {
 	sqs.receiveMessage({
 		QueueUrl: 'https://sqs.us-east-1.amazonaws.com/480964559519/Hiru_test.fifo',
 		AttributeNames: ['All'],
-		MaxNumberOfMessages: '1',
+		MaxNumberOfMessages: '2',
 		VisibilityTimeout: '30',
 		WaitTimeSeconds: '0'
 	}, function (receivedMessages) {
 		receivedMessages.forEach(message => {
 			// your logic to access each message through out the loop. Each message is available under variable message 
-			// within this block
+			// within this block test
 		})
 	}, function (error) {
 		// implement error handling logic here
