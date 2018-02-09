8aa2bef0e928d2a800fe3686f5c24e83fe73d959
hirudinee <35219641+hirudinee@users.noreply.github.com>
2018-02-09 13:02:37 +0530

    sample

--- Hiru_SQS/lambda.js
+++ Hiru_SQS/lambda.js
@@ -3,28 +3,7 @@ let SL = require('@slappforge/slappforge-sdk');
 const sqs = new SL.AWS.SQS(AWS);
 exports.handler = function (event, context, callback) {
 
-	sqs.sendMessage({
-		MessageBody: 'This is a 0.6.0. test edit',
-		QueueUrl: 'https://sqs.us-east-1.amazonaws.com/480964559519/Hiru_test.fifo',
-		DelaySeconds: '0',
-		MessageDeduplicationId: '001',
-		MessageGroupId: 'Sample',
-		MessageAttributes: {
-			"test": {
-				"DataType": "Number",
-				"StringValue": "001"
-			}
-		}
-	}, function (data) {
-		// your logic (logging etc) to handle successful message delivery, should be here
-		// test
-		console.log(data);
-		callback(null, data);
-	}, function (error) {
-		// your logic (logging etc) to handle failures, should be here test comment test samep
-		console.log(error);
-		callback(null, error);
-	});
+	
 
 	//callback(null, 'Successfully executed');
 }
--- Hiru_SQS/lanbda.js
+++ Hiru_SQS/lanbda.js
@@ -1,6 +1,23 @@
 let AWS = require('aws-sdk');
-exports.handler = function(event, context, callback) {
+let SL = require('@slappforge/slappforge-sdk');
+const sqs = new SL.AWS.SQS(AWS);
+exports.handler = function (event, context, callback) {
+	sqs.receiveMessage({
+		QueueUrl: 'https://sqs.us-east-1.amazonaws.com/480964559519/Hiru_test.fifo',
+		AttributeNames: ['All'],
+		MaxNumberOfMessages: '1',
+		VisibilityTimeout: '30',
+		WaitTimeSeconds: '0'
+	}, function (receivedMessages) {
+		receivedMessages.forEach(message => {
+			// your logic to access each message through out the loop. Each message is available under variable message 
+			// within this block
+		})
+	}, function (error) {
+		// implement error handling logic here
+		//test
+	});
 
 
-	callback(null,'Successfully executed');
+	callback(null, 'Successfully executed');
 }
